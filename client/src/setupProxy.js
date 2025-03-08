ce4cf65ff781b49688645544c358049622e0a3bc
Swapn <agarwalswapn@gmail.com>
2025-03-08 15:49:42 +0530

    fix: remove duplicate proxy configuration

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,17 +5,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   // Double-check we are in development
   if (process.env.NODE_ENV !== 'production') {
-    console.log('Setting up development proxy to localhost:3001');
+    console.log('Setting up development proxy for /api routes to localhost:3001');
     app.use(
       '/api',
       createProxyMiddleware({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
+        pathRewrite: {
+          '^/api': '/api' // keep the /api prefix
+        },
         onError: (err, req, res) => {
           console.error('Proxy Error:', err);
           res.status(500).send('Proxy Error');
-        }
+        },
+        logLevel: 'debug' // add more detailed logging
       })
     );
   } else {
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         },
         headers: {
             'Access-Control-Allow-Origin': '*',
-        }
+        },
+        proxy: null
     }
 }; 
