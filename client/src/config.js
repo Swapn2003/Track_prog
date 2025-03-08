c9c3995af232eadf329a00253ad714dcbc34b01f
Swapn <agarwalswapn@gmail.com>
2025-03-08 06:59:10 +0530

    Update API URL for production

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    API_URL: 'http://localhost:3001'
+    API_URL: process.env.NODE_ENV === 'production' 
+        ? 'https://track-prog-backend.onrender.com'
+        : 'http://localhost:3001'
 };
 
 export default config; 
